09af8843f6dff12099a4e931d7d6f60be292aa4c
Rahul Kumar <cserahulkumar@gmail.com>
2017-12-30 16:48:41 +0530

    Renamed THe root of ui-- e was missing

--- onlineshopping_ui/src/main/java/com/rahul/onlineshopping_ui/controller/PageController.java
+++ onlineshopping_ui/src/main/java/com/rahul/onlineshopping_ui/controller/PageController.java
@@ -1,10 +1,7 @@
-package com.rahul.onlinshopping_ui.controller;
+package com.rahul.onlineshopping_ui.controller;
 
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
